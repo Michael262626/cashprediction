49711059b248d553c82f08b97cbfd5be1524e43f
Michael262626 <dikandumichael@gmail.com>
2025-08-02 10:02:05 +0100

    Commit

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 "use client"
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api/v1"
+const API_BASE_URL = `${process.env.NEXT_PUBLIC_API_URL}/api/v1` || "http://localhost:8000/api/v1"
 
 interface ATMData {
   id: string
